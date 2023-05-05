db3984afeb3c1136febd109d5467602d2ebd94cc
SAEMMM <yangpa2014@gmail.com>
2023-05-05 20:14:51 +0900

    [feat] font 추가

--- src/season/StSeason.js
+++ src/season/StSeason.js
@@ -6,19 +6,27 @@ height: 400px;
 background-color: navy;
 -webkit-box-shadow: 5px 5px 12px 2px #707070; 
 box-shadow: 5px 5px 19px 2px #707070;
+border-radius: 5px;
+filter: brightness(0.7);
 cursor: pointer;
 .SeasonBoxp {
-  font-size: 40px;
+  font-size: 50px;
   font-weight: bolder;
   color: white;
   text-align: center;
   line-height: 300px;
-  text-shadow: 0px 0px 10px ${props => props.season === '봄' ? '#BE0B9C' : (props => props.season === '여름' ? '#006EBE' : (props => props.season === '가을' ? '#A9530B' : '#FFFFFF') ) };
+  text-shadow: 0px 0px 10px ${props => props.season === '봄' ? '#EC5196' : (props => props.season === '여름' ? '#006EBE' : (props => props.season === '가을' ? '#A9530B' : '#FFFFFF') ) };
+  
 }
 &:hover {
   width: 240px;
   height: 430px;
+  filter: brightness(1);
   transition: all 0.5s;
+  .SeasonBoxp {
+    font-size: 60px;
+    transition: all 0.5s;
+  }
 }
 `
 
@@ -39,4 +47,5 @@ height: 600px;
 display: flex;
 align-items: center;
 justify-content: space-between;
+font-family: 'Gowun Batang', serif;
 `
